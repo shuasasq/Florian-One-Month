34ca80ecb9db99f72e574c1d9d94fb65d24bff31
shuasasq <santiago.huasasquiche@gmail.com>
2026-08-19 07:35:03 -0500

    Wire photo album into surprise

--- assets-apply.js
+++ assets-apply.js
@@ -1,10 +1,15 @@
-const pic=(window.FLORIAN_IMAGES||[])[0];
-if(pic){
-  document.querySelectorAll('.polaroid img').forEach(img=>img.src=pic);
-  const hero=document.querySelector('.hero-photo');
-  const ending=document.querySelector('.ending-photo');
-  const letter=document.querySelector('.letter-backdrop');
-  if(hero) hero.style.backgroundImage=`url("${pic}")`;
-  if(ending) ending.style.backgroundImage=`url("${pic}")`;
-  if(letter) letter.style.backgroundImage=`linear-gradient(rgba(15,11,24,.73),rgba(15,11,24,.94)),url("${pic}")`;
+const pics = window.FLORIAN_IMAGES || [];
+const galleryImages = [...document.querySelectorAll('.polaroid img')];
+galleryImages.forEach((img, index) => {
+  if (pics[index]) img.src = pics[index];
+});
+
+const hero = document.querySelector('.hero-photo');
+const ending = document.querySelector('.ending-photo');
+const letter = document.querySelector('.letter-backdrop');
+
+if (hero && pics[1]) hero.style.backgroundImage = `url("${pics[1]}")`;
+if (ending && pics[3]) ending.style.backgroundImage = `url("${pics[3]}")`;
+if (letter && pics[2]) {
+  letter.style.backgroundImage = `linear-gradient(rgba(15,11,24,.73),rgba(15,11,24,.94)),url("${pics[2]}")`;
 }
